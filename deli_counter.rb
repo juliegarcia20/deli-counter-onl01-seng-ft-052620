c9941bc4864f0dc413520abf37bb6385ed6a7dd4
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 18:58:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,10 @@ def take_a_number(array, name)
 end 
 
 def now_serving(array)
+  
+if array.length > 0 
+  puts "Currently serving #{array.shift}."
+else 
   puts "There is nobody waiting to be served!"
  end 
 end 
