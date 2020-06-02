d06006817f49a41e19b98e5ea6ba33025da6901d
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 18:53:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
 # Write your code here.
 def line(array)
 
-array.map!.with_index(1) do |name, index|
+ array.map!.with_index(1) do |name, index|
   "#{index}. #{name}"
 end 
 
 if array.length > 0 
   puts "The line is currently: #{array.join("")}"
+else 
+  puts "The line is currently empty."
  end 
 end 
 
