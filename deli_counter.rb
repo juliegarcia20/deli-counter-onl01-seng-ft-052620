2060cefec250895008af0b6f0df114f5ea42ce5f
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 18:59:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
 end 
 
 if array.length > 0 
-  puts "The line is currently: #{array.join("")}"
+  puts "The line is currently: #{array.join ("")}"
 else 
   puts "The line is currently empty."
  end 
